2f624b2e08ca92f7a8a336cd29928f9ff7734995
Sam <screasey97@googlemail.com>
2020-11-15 14:54:26 +0100

    modified dropdown

--- src/components/UI/Expandable.js
+++ src/components/UI/Expandable.js
@@ -1,16 +1,15 @@
 import React, {useState} from 'react';
+import Collapsible from 'react-collapsible';
 import styles from './Expandable.module.scss';
 
-const Expandable = (props) => {
-    const [expanded, set] = useState(false)
+const Expandable = ({header, classes, children}) => {
     return (
-        <div className={props.classes + " " + styles.expandable} style={{boxShadow:"-0px -4px 5px rgba(0,0,0,0.2)"}}>
-        <h4 onClick={()=>set(!expanded)} className="Bold" style={{cursor:'pointer'}}>{props.header}<span className={styles.arrow_down} style={{transform:expanded ? "rotateZ(180deg)" : "none"}}></span></h4>
-                <div style={{transition:"0.5s ease-out", maxHeight:expanded ? "500px" 
-                : "0px", overflow:"hidden"}} className={styles.collapsible}>
-                    {props.children}
+        <Collapsible classParentString={classes + " Collapsible"} timeout={250} trigger={<h4 className={"Bold "} style={{cursor:'pointer'}}>{header}<span className={styles.arrow_down}></span></h4>}>
+        
+                <div className={styles.collapsible}>
+                    {children}
                 </div>
-        </div>
+        </Collapsible>
     )
 }
 
